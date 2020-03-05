a72afdf1718b8aed34d1c7e64729d81e05d8c78b
Wander128 <wanderleybc128@gmail.com>
2020-03-04 21:07:54 -0500

    2dfgdf

---
+++ Assets/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Destroy : MonoBehaviour
+{   public Text 
+    int numEnem;
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Player") {
+            Destroy(collision.gameObject);
+            numEnem=
+
+        }
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject Enemigo;
+    float Counter;
+    public float emision = 0.5f;
+    public float MinR = -8;
+    public float MaxR = 8;
+
+    Vector3 rangoRandom;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Counter = Counter + Time.deltaTime;
+        if (Counter > emision)
+        {
+            Counter = 0;
+            rangoRandom = new Vector3(transform.position.x, transform.position.y,
+            Random.Range(MinR, MaxR));
+            Instantiate(Enemigo, rangoRandom, transform.rotation);
+        }
+
+    }
+}
